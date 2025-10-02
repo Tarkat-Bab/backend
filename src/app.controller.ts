1e5cbe18b700d10704fe477bbc88fe2bc29749fd
rewan-adel <rewanadel1266@gmail.com>
2025-10-02 08:33:46 +0300

    fix: update project name in package.json and enhance public access in controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { isPublic } from './common/decorators/public.decorator';
 
+@isPublic()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { isPublic } from './common/decorators/public.decorator';
 
+@isPublic()
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello from Tarket Bab API!';
   }
 }
