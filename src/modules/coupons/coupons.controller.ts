4a267c1a5b0a4095478865301eabe165dde6063a
rewan-adel <rewanadel1266@gmail.com>
2025-11-30 19:52:00 +0200

    first-order-discount

--- src/modules/coupons/coupons.controller.ts
+++ src/modules/coupons/coupons.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch } from '@nestjs/common';
+import { CouponsService } from './coupons.service';
+import { UpdateFirstOrderDiscountDto } from './dtos/update-first-order-discount.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-@Controller('coupons')
-export class CouponsController {}
+@ApiBearerAuth()
+@ApiTags('first-order-discount')
+@Controller('first-order-discount')
+export class CouponsController {
+    constructor(private readonly couponsService: CouponsService) {}
+
+    @Get('first-order-discount')
+    async getFirstOrderDiscount() {
+        return this.couponsService.getFirstOrderDiscount();
+    }
+}
